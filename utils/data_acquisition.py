146c0c11f08e24510bbca78f9faedfee1d816cb3
jantiegges <jantiegges@gmail.com>
2023-10-16 23:24:20 -0400

    finish data acquisition

--- utils/data_acquisition.py
+++ utils/data_acquisition.py
@@ -1,40 +1,32 @@
 from torchvision import datasets, transforms
+from sklearn.preprocessing import OneHotEncoder
 
+def load_and_preprocess_data(data_dirname, dataset_name, normalize=True):
+    # define a transform to normalize the data
+    if normalize:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+    else:
+        transform = transforms.ToTensor()
 
-def load_fashion_mnist(data_dirname):
-    #transform = transforms.Compose([transforms.ToTensor(),
-    #                                transforms.Normalize((0.5,), (0.5,), )])
+    if dataset_name == 'F_MNIST':
+        trainset = datasets.FashionMNIST(data_dirname, download=True, train=True, transform=transform)
+        testset = datasets.FashionMNIST(data_dirname, download=True, train=False, transform=transform)
+    elif dataset_name == 'CIFAR10':
+        trainset = datasets.CIFAR10(data_dirname, download=True, train=True, transform=transform)
+        testset = datasets.CIFAR10(data_dirname, download=True, train=False, transform=transform)
 
-    trainset = datasets.FashionMNIST(data_dirname, download=True, train=True)
-    testset = datasets.FashionMNIST(data_dirname, download=True, train=False)
+    # flatten and scale 
+    X_train = trainset.data.numpy().reshape(-1, 28*28) / 255.0
+    y_train = trainset.targets.numpy()
+    X_test = testset.data.numpy().reshape(-1, 28*28) / 255.0
+    y_test = testset.targets.numpy()
 
-    return trainset, testset
+    # one-hot encode
+    encoder = OneHotEncoder(sparse_output=False, categories='auto')
+    y_train_oh = encoder.fit_transform(y_train.reshape(-1, 1))
+    y_test_oh = encoder.transform(y_test.reshape(-1, 1))
 
-
-def load_stl10(data_dirname):
-    transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.Normalize((0.5,), (0.5,), )])
-
-    trainset = datasets.STL10(data_dirname, download=True, train=True, transform=transform)
-    testset = datasets.STL10(data_dirname, download=True, train=False, transform=transform)
-
-    return trainset, testset
-
-
-from sklearn.model_selection import train_test_split
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-
-def load_wine_dataset():
-
-    wine_df = pd.read_csv('./data/wine.csv')
-    one_hot_wine_classes = pd.get_dummies(wine_df['class']).to_numpy(dtype=int)
-
-    X_wine_train, X_wine_test, y_wine_train, y_wine_test = train_test_split(wine_df.drop(['class'], axis=1).to_numpy(),
-                                                                            one_hot_wine_classes, test_size=0.1)
-
-    scalar = StandardScaler().fit(X_wine_train)
-    X_wine_train = scalar.transform(X_wine_train)
-    X_wine_test = scalar.transform(X_wine_test)
-
-    return X_wine_train, y_wine_train
+    return X_train, X_test, y_train_oh, y_test_oh
