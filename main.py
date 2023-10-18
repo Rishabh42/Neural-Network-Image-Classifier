244af4f16a56e4c155b294ce681e784aee361cc5
jonathan <jonathancolacocarr@gmail.com>
2023-10-18 17:20:07 -0400

    Add DataLoader outside of preprocess function

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ from models.cnn import CNN
 from utils.data_acquisition import load_and_preprocess_data
 from utils.plotting import compare_training_histories, plot_training_history, compare_accuracies
 
+
 def exp1(optimizer_kwargs, optimizer_name, filepath='./out/exp1/', 
          epochs=100, batch_size=256, verbose=True):
     
-    X_train, X_test, y_train_oh, y_test_oh = load_and_preprocess_data('./data/F_MNIST_data', dataset_name='F_MNIST')
+    X_train, X_test, y_train_oh, y_test_oh = load_and_preprocess_data('./data/CIFAR10_data', dataset_name='CIFAR10')
     
     initializers = {'Zeros': all_zeros, 'Uniform': uniform, 'Gaussian': gaussian, 'Xavier': xavier_uniform, 'Kaiming': kaiming}
     histories = []
@@ -64,6 +65,7 @@ def exp1(optimizer_kwargs, optimizer_name, filepath='./out/exp1/',
 
     return histories, final_accuracies
 
+
 def exp6(optimizer_kwargs, filepath='./out/exp6', stride=1, kernel=3, 
          padding=1, epochs=100, batch_size=16, verbose=True):
     """Implement CNN with Pytorch"""
@@ -78,7 +80,9 @@ def exp6(optimizer_kwargs, filepath='./out/exp6', stride=1, kernel=3,
 
 def exp6_grid_search(param_grid, filepath='./out/exp6/grid_search', verbose=False):
 
-    trainset, testset = load_and_preprocess_data('./data/F_MNIST_data', dataset_name='F_MNIST', normalize=True, mlp=False)
+    X_train, X_test, y_train, y_test = load_and_preprocess_data('./data/F_MNIST_data', dataset_name='F_MNIST', normalize=True, mlp=False)
+    trainset = torch.utils.data.TensorDataset(X_train, y_train)
+    testset = torch.utils.data.TensorDataset(X_test, y_test)
 
     histories = []
     final_accuracies = []
@@ -98,7 +102,7 @@ def exp6_grid_search(param_grid, filepath='./out/exp6/grid_search', verbose=Fals
         testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
 
         # init model
-        model = CNN(input_channels=1, image_size=28, conv1_out=conv1_out, conv2_out=conv2_out, stride=stride, 
+        model = CNN(input_channels=X_train.shape[1], image_size=X_train.shape[2], conv1_out=conv1_out, conv2_out=conv2_out, stride=stride,
                     kernel_size=kernel_size, padding=padding, optimizer=optimizer_name, **optimizer_kwargs)
 
         # train
@@ -158,17 +162,17 @@ if __name__ == '__main__':
         'epochs': [5]
     }
 
-    # param_grid = {
-    #     'conv1_out': [16],
-    #     'conv2_out': [32],
-    #     'stride': [1],
-    #     'kernel_size': [3],
-    #     'padding': [1],
-    #     'optimizer': ['Adam'],
-    #     'lr': [0.001],
-    #     'batch_size': [16],
-    #     'epochs': [1]
-    # }
+    param_grid = {
+        'conv1_out': [16],
+        'conv2_out': [32],
+        'stride': [1],
+        'kernel_size': [3],
+        'padding': [1],
+        'optimizer': ['Adam'],
+        'lr': [0.001],
+        'batch_size': [16],
+        'epochs': [1]
+    }
 
     exp6_grid_search(param_grid, verbose=True)
 
